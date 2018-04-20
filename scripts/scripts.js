0da1b38b6f7f9f9c387ec4594c9fb69582657f04
Dan McGuane <daniel.mcguane@evertrue.com>
2018-04-20 15:43:21 -0400

    use let ya bum

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,13 +21,13 @@ function swapBothImage() {
 }
 
 function dragStart(event) {
-  var style = window.getComputedStyle(event.target, null);
+  let style = window.getComputedStyle(event.target, null);
   event.dataTransfer.setData("text/plain",
   (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
 } 
 
 function drop(event) {
-  var offset = event.dataTransfer.getData("text/plain").split(',');
+  let offset = event.dataTransfer.getData("text/plain").split(',');
   bothImage.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
   bothImage.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
   event.preventDefault();
