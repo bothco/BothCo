9a51d53c516a505eef5cd9febb2085a396fa8692
Dan McGuane <daniel.mcguane@evertrue.com>
2018-04-20 15:30:18 -0400

    fix dat style

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,21 +21,21 @@ function swapBothImage() {
 }
 
 function dragStart(event) {
-    var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+  var style = window.getComputedStyle(event.target, null);
+  event.dataTransfer.setData("text/plain",
+  (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
 } 
 
 function drop(event) {
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    var dm = document.getElementById('both-img');
-    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-    event.preventDefault();
-    return false;
+  var offset = event.dataTransfer.getData("text/plain").split(',');
+  var dm = document.getElementById('both-img');
+  dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+  dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+  event.preventDefault();
+  return false;
 }
 
 function dragOver(event) {
-    event.preventDefault();
-    return false;
+  event.preventDefault();
+  return false;
 }
